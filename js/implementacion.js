c961fe1ecc986697b14d7adbda28aa5160e091eb
Erik Cori <erik199666@hotmail.com>
2019-05-24 12:23:58 -0300

    arreglos

--- js/implementacion.js
+++ js/implementacion.js
@@ -1,34 +1,69 @@
 
 var url = "https://api.propublica.org/congress/v1/113/senate/members.json";
 
+	
+
 fetch(url, {
 		headers: {
-			"X-API-KEY": "vqwpr2eNBwzsoorSDYeNzcvBobBHoVdbvkINnMP4"
+			"X-API-KEY":"vqwpr2eNBwzsoorSDYeNzcvBobBHoVdbvkINnMP4"
 		}
 	})
+	.then(function (myData){
+	return myData.json();
+})
 	.then(function (myData) {
-		return myData.json();
-		
-	})
-	.then(function (myData) {
-		var data = myData;
-		console.log(myData)
+		data = myData;	
+		implementarDatos()
 	})
 
+function implementarDatos() {
+	//Array de miembros
+	var miembros = data.results[0].members;
+	//para el caso de un cambio en los checkboxes
+	var checkBoxes = document.querySelectorAll('input[name=party]');
+	checkBoxes.forEach(input => input.onchange = filtrarYMostrarTabla);
+	//Array de estados
+	var estados = miembros.map(miembro => miembro.state);
+	//ordenamiento del array de estados
+	estados = estados.filter((estado, i, array) => array.indexOf(estado) === i);
+	estados.sort();
 
-//Array de miembros
-var miembros = data.results[0].members;
-//para el caso de un cambio en los checkboxes
-var checkBoxes = document.querySelectorAll('input[name=party]');
-checkBoxes.forEach(input => input.onchange = filtrarYMostrarTabla);
-//Array de estados
-var estados = miembros.map(miembro => miembro.state);
-//ordenamiento del array de estados
-estados = estados.filter((estado, i, array) => array.indexOf(estado) === i);
-estados.sort();
+	//llamo a la funcion que crea la lista de estados del dropdown
+	listaDropdownEstados(estados);
 
-//llamo a la funcion que crea la lista de estados del dropdown
-listaDropdownEstados(estados);
+	//llamo a la funcion que crea la tabla 
+	filtrarYMostrarTabla();
+	
+	//funcion para filtrar y mostrar la tabla
+function filtrarYMostrarTabla(){
+	var checkBoxesSelec = document.querySelectorAll('input[name=party]:checked');
+	var partidosSelec = Array.from(checkBoxesSelec).map(element => element.value);
+	
+	var filtradoPorPartido = miembro => {
+    if(partidosSelec.indexOf(miembro.party) > -1){
+      return miembro;
+    }
+  }
+	miembrosFiltrados = miembros.filter(filtradoPorPartido);
+	
+	var estadoSelec = document.getElementById("dropStates").value;
+  if (estadoSelec != ""){
+    miembrosFiltrados = miembrosFiltrados.filter(miembro => miembro.state == estadoSelec);
+  }
+	crearTablaEnHtml(miembrosFiltrados);
+}
 
-//llamo a la funcion que crea la tabla 
-filtrarYMostrarTabla();
+//funcion que crea la lista de estados del dropdown
+function listaDropdownEstados(estados){
+  var listaDropdown = document.getElementById("dropStates");
+
+  listaDropdown.onchange = filtrarYMostrarTabla;
+
+  estados.forEach(estado => {
+    var opcion = document.createElement("option");
+    opcion.value = estado;
+    opcion.innerText = estado;
+    listaDropdown.appendChild(opcion);
+  })
+}
+}
